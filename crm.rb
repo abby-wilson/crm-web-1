663ed61dc669263914c420d29c2215987ee9671d
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-12 17:59:36 -0400

    Contacts and landing pages done

--- crm.rb
+++ crm.rb
@@ -2,5 +2,17 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+  @contacts_page = "://localhost:4567/contacts"
   erb :index
+end
+
+get '/contacts' do
+  @landing_page = "://localhost:4567/"
+  @all_contacts = Contact.all
+  erb :contacts
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
 end
